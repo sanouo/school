d8c78470923a6fa3c1bba0cf379659336816e9ff
sanouo <sanouo591@gmail.com>
2018-05-17 16:12:01 +0200

    first commit

---
+++ views/template/footer.php
@@ -0,0 +1,26 @@
+<!-- start of the footer -->
+        <footer>
+          <ul>
+            <li>Legal Notice</li>
+            <li>Privacy Policy</li>
+            <li>Copyright © CarDistrib</li>
+          </ul>
+        </footer>
+<!-- end of the footer -->
+
+
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+       <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+       <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js" integrity="{{JQUERY_SRI_HASH}}" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>')</script>
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
---
+++ views/template/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+    <html class="no-js" lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <title>School</title>
+            <meta name="description" content="application for stock management for the piece auto">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <link rel="apple-touch-icon" href="apple-touch-icon.png">
+            <link rel="stylesheet" href="assets/css/bootstrap.css">
+            <link rel="stylesheet" href="assets/css/normalize.css">
+            <link rel="stylesheet" href="assets/css/main.css">
+            <link rel="icon" href="favicon1.ico">   
+        </head>
+        <body>
